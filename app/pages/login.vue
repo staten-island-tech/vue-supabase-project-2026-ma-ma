0dcb25cc0a8260736cb6ba80a61d3320c4538c38
v81d <v81d@proton.me>
2026-04-29 13:57:34 -0400

    feat(login): add ButtonSecondary to login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,9 +2,10 @@
   <div class="flex justify-center items-center p-32 w-screen h-screen bg-black">
     <!-- Login card -->
     <div
-      class="flex justify-center items-center p-16 w-full max-w-xl h-full rounded bg-white"
+      class="flex justify-center items-center gap-5 p-16 w-full max-w-xl h-full bg-white"
     >
-      <ButtonPrimary label="log in" class="w-full" />
+      <ButtonSecondary label="secondary" class="w-full" />
+      <ButtonPrimary label="primary" class="w-full" />
     </div>
   </div>
 </template>
