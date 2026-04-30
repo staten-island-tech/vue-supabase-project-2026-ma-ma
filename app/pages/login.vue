eb65cbbca220c141cb39701f7385e2b93b8c679f
hi <115565725+Mikeblackbelt@users.noreply.github.com>
2026-04-30 14:10:10 -0400

    feat: basic login setup

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -4,9 +4,8 @@
     <div
       class="flex justify-center items-center p-16 w-full max-w-xl h-full rounded bg-white"
     >
-      <ButtonPrimary label="log in" class="w-full" />
-      <!--<LoginBackground />-->
-      
+      <ButtonSecondary label="secondary" class="w-full" />
+      <ButtonPrimary label="primary" class="w-full" />
     </div>
   </div>
 </template>
