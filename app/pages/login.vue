42d3e329f4e7280bf9d81389cf9f7a2e0714826b
hi <115565725+Mikeblackbelt@users.noreply.github.com>
2026-04-29 20:04:09 -0400

    its ok for now but needs to be improved

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -5,6 +5,7 @@
       class="flex justify-center items-center p-16 w-full max-w-xl h-full rounded bg-white"
     >
       <ButtonPrimary label="log in" class="w-full" />
+      <LoginBackground />
     </div>
   </div>
 </template>
