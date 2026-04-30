717811f7abdaf6d865fda967da897ceaa1f9ddc5
v81d <v81d@proton.me>
2026-04-30 17:50:03 -0400

    feat: create text input box component

---
+++ app/components/TextInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input
+      :type="props.type"
+      :placeholder="props.placeholder"
+      v-bind="$attrs"
+      :class="[
+        'p-5',
+        'outline-none',
+        'border-4 border-double border-input-border bg-input',
+        'text-xl text-input-foreground',
+        'transition-all duration-150',
+        'focus:border-input-focus-border focus:bg-input-focus focus:text-input-focus-foreground',
+        'placeholder:text-input-placeholder',
+      ]"
+    >
+    </input>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    type: string;
+    placeholder: string;
+  }>()
+</script>
+
+<style scoped></style>
