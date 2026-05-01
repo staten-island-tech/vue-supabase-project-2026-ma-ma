5ba8ea8657d4f6ba991405cd4ee0d49651e3520a
v81d <v81d@proton.me>
2026-05-01 15:44:27 -0400

    feat(login-page): update login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,7 +2,7 @@
   <DomainBackground class="absolute -z-10" />
   <div class="flex justify-center items-center p-32 w-screen h-screen">
     <Card>
-      <h2 class="text-xl font-medium text-slate-500">Sign In</h2>
+      <h2 class="text-xl font-medium text-slate-500">Log in to your account</h2>
 
       <TextInput
         label="Username"
@@ -14,14 +14,11 @@
       <TextInput
         label="Password"
         type="password"
-        placeholder="••••••••"
+        placeholder="••••••••••••"
         class="w-full"
       />
 
-      <div class="flex flex-row-reverse gap-5">
-        <ButtonPrimary label="Sign In" class="flex-1" />
-        <ButtonSecondary label="Register" class="flex-1" />
-      </div>
+      <ButtonPrimary label="Log In" />
     </Card>
   </div>
 </template>
@@ -29,4 +26,3 @@
 <script setup lang="ts"></script>
 
 <style scoped></style>
-
