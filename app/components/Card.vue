d9b8d8d0e00c9145c7bc43740e3f24fdf3faa067
v81d <v81d@proton.me>
2026-05-01 16:42:04 -0400

    feat(card): increase card max width

--- app/components/Card.vue
+++ app/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col gap-5 p-10 w-full max-w-md border-double border-8 border-card-border ring-4 ring-card-ring ring-inset bg-card text-card-foreground shadow-transparent-lg"
+    class="flex flex-col gap-5 p-10 w-full max-w-lg border-double border-8 border-card-border ring-4 ring-card-ring ring-inset bg-card text-card-foreground shadow-transparent-lg"
   >
     <slot />
   </div>
