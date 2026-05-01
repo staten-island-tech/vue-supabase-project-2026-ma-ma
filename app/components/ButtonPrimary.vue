dc77212edfb08c553e4afbf3343a3871ef936181
v81d <v81d@proton.me>
2026-05-01 15:36:12 -0400

    feat(buttons): use scale instead of brightness when active

--- app/components/ButtonPrimary.vue
+++ app/components/ButtonPrimary.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <button
       v-bind="$attrs"
-      class="w-full px-4 py-2 border-4 border-double border-primary-border bg-primary text-md text-primary-foreground transition-all duration-150 hover:border-primary-hover hover:bg-primary-hover hover:text-primary-hover-foreground active:bg-primary-hover active:border-primary-hover active:text-primary-hover-foreground active:brightness-90"
+      class="w-full px-4 py-2 border-4 border-double border-primary-border bg-primary text-md text-primary-foreground transition-all duration-150 hover:border-primary-hover hover:bg-primary-hover hover:text-primary-hover-foreground active:bg-primary-hover active:border-primary-hover active:text-primary-hover-foreground active:scale-95"
     >
       {{ props.label }}
     </button>
